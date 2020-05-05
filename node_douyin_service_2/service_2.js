7bf388ed08027e176dca282e4a2fb291c6a5db3a
william <1243650225@qq.com>
2020-05-05 16:04:37 +0800

    增加node_douyin_service_2 /data接口可获得完整的数据

---
+++ node_douyin_service_2/service_2.js
@@ -0,0 +1,162 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const get_sign = require('./douyin_2');
+const superagent = require('superagent');
+// var bodyParser = require('body-parser');
+
+const app = express();
+// app.use(bodyParser());
+
+var browser;
+var page;
+
+
+
+(async () => {
+    // 初始化
+    browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    page = await browser.newPage();
+    // 去除webdriver
+    await page.evaluateOnNewDocument(() => {
+        const newProto = navigator.__proto__;
+        delete newProto.webdriver;
+        navigator.__proto__ = newProto;
+    });
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36');
+    console.log('初始化完毕');
+})();
+
+// http://localhost:5000/user?uid=102064772608
+app.get('/user', async function (req, res) {
+    try {
+        console.log(req.url);
+        var uid = req.query.uid;
+        // 特殊字符存在，代码请求的tac有问题
+        var tac_ = '|ms g,)gk}ejo{\x7fcms!g,&Iebli\x7fms"l!,)~oih\x7fgyucmk"t (\x80,.jjvx|vDgyg}knbl"d"inkfl"v,.jjvx|vDgyg}knbmxl!,)~oih\x7fgyucgr&Objectn vuq%valuevfq(writable[#c}) %{s#t ,4KJarz}hrjxl@EWCOQDRB,3LKfs{}wsnqB{iAMWBP@,;DCj{}DSKUAWyTK[C[XrHZ^RFZ[[,7HGn\x7fyxowiES}PGWOW\\vL^BN,5JI`}{~iuk{m\x7fRAQMURxNG,3LKsnsjpl~nB{iAMWBP@,2MLpg\x7fa}kEnrjl~PQGG,5JI`}{~iuk{m\x7fTLTVDVWMM,1NMwf|`rjF\x7fm}qk~TD,4KJert|tripAjNVPBTUCC,4KJpo|ksmyoAjNVPBTUCC[+s#,)Vyn`h`fe|,,olbcCt~vz|cz,6ID}u\x7fuuhs@ieg|v@EHZMOY[#s$l$*%s%l%u&k4s&l$l&ms\'l l\'mk"t j\x06l#*%s%l%u&k?s&l#l&ms\'l ,(lfi~ah`{ml\'mk"t j\ufffbl ,(lfi~ah`{m*%s%l%u&kls&l&vr%matchxgr&RegExp$*\\$[a-z]dc_$ n"[!cvk:}l ,(lfi~ah`{ml&m,&efkaoTmk"t j\uffcef z[ cb|1d<,%Dscafgd"in,8[xtm}nLzNEGQMKAdGG^NTY\x1ckgd"inb<b|1d<g,&TboLr{m,(\x02)!jx-2n&vr$testxg,%@tug{mn ,%vrfkbm[!cb|'
+        // var tac_ = '|ms g,)gk}ejo{\x7fcms!g,&Iebli\x7fms"l!,)~oih\x7fgyucmk"t (\x80,.jjvx|vDgyg}knbl"d"inkfl"v,.jjvx|vDgyg}knbmxl!,)~oih\x7fgyucgr&Objectn vuq%valuevfq(writable[#c}) %{s#t ,4KJarz}hrjxl@EWCOQDRB,3LKfs{}wsnqB{iAMWBP@,;DCj{}DSKUAWyTK[C[XrHZ^RFZ[[,7HGn\x7fyxowiES}PGWOW\\vL^BN,5JI`}{~iuk{m\x7fRAQMURxNG,3LKsnsjpl~nB{iAMWBP@,2MLpg\x7fa}kEnrjl~PQGG,5JI`}{~iuk{m\x7fTLTVDVWMM,1NMwf|`rjF\x7fm}qk~TD,4KJert|tripAjNVPBTUCC,4KJpo|ksmyoAjNVPBTUCC[+s#,)Vyn`h`fe|,,olbcCt~vz|cz,6ID}u\x7fuuhs@ieg|v@EHZMOY[#s$l$*%s%l%u&k4s&l$l&ms\'l l\'mk"t j\x06l#*%s%l%u&k?s&l#l&ms\'l ,(lfi~ah`{ml\'mk"t j\ufffbl ,(lfi~ah`{m*%s%l%u&kls&l&vr%matchxgr&RegExp$*\\$[a-z]dc_$ n"[!cvk:}l ,(lfi~ah`{ml&m,&efkaoTmk"t j\uffcef z[ cb|1d<,%Dscafgd"in,8[xtm}nLzNEGQMKAdGG^NTY\x1ckgd"inb<b|1d<g,&TboLr{m,(\x02)!jx-2n&vr$testxg,%@tug{mn ,%vrfkbm[!cb|'
+        var _tac = await getTac(uid);
+        // console.log(_tac)
+        if (_tac == null) {
+            res.send(`{"code":-1,"context":"Fail to get tac.","reason":"No such uid ${uid}.Try again if you sure uid is correct."}`);
+        } else {
+            tac = _tac + tac_;
+            // console.log(tac)
+            // var tac = 'i)6a5my560cs!i#158s"0y\u02a1g,&qnfme|ms g,)gk}ejo{\x7fcms!g,&Iebli\x7fms"l!,)~oih\x7fgyucmk"t (\x80,.jjvx|vDgyg}knbl"d"inkfl"v,.jjvx|vDgyg}knbmxl!,)~oih\x7fgyucgr&Objectn vuq%valuevfq(writable[#c}) %{s#t ,4KJarz}hrjxl@EWCOQDRB,3LKfs{}wsnqB{iAMWBP@,;DCj{}DSKUAWyTK[C[XrHZ^RFZ[[,7HGn\x7fyxowiES}PGWOW\\vL^BN,5JI`}{~iuk{m\x7fRAQMURxNG,3LKsnsjpl~nB{iAMWBP@,2MLpg\x7fa}kEnrjl~PQGG,5JI`}{~iuk{m\x7fTLTVDVWMM,1NMwf|`rjF\x7fm}qk~TD,4KJert|tripAjNVPBTUCC,4KJpo|ksmyoAjNVPBTUCC[+s#,)Vyn`h`fe|,,olbcCt~vz|cz,6ID}u\x7fuuhs@ieg|v@EHZMOY[#s$l$*%s%l%u&k4s&l$l&ms\'l l\'mk"t j\x06l#*%s%l%u&k?s&l#l&ms\'l ,(lfi~ah`{ml\'mk"t j\ufffbl ,(lfi~ah`{m*%s%l%u&kls&l&vr%matchxgr&RegExp$*\\$[a-z]dc_$ n"[!cvk:}l ,(lfi~ah`{ml&m,&efkaoTmk"t j\uffcef z[ cb|1d<,%Dscafgd"in,8[xtm}nLzNEGQMKAdGG^NTY\x1ckgd"inb<b|1d<g,&TboLr{m,(\x02)!jx-2n&vr$testxg,%@tug{mn ,%vrfkbm[!cb|'
+            // await page.goto(`https://www.iesdouyin.com/share/user/${uid}`);
+            var result = await get_signature_for_get(uid, tac);
+            res.send(result);
+        }
+
+    } catch (e) {
+        res.send(e);
+    }
+})
+
+app.get('/data', async function (req, res) {
+    try {
+        console.log(req.url);
+        var uid = req.query.uid;
+        var tac_ = '|ms g,)gk}ejo{\x7fcms!g,&Iebli\x7fms"l!,)~oih\x7fgyucmk"t (\x80,.jjvx|vDgyg}knbl"d"inkfl"v,.jjvx|vDgyg}knbmxl!,)~oih\x7fgyucgr&Objectn vuq%valuevfq(writable[#c}) %{s#t ,4KJarz}hrjxl@EWCOQDRB,3LKfs{}wsnqB{iAMWBP@,;DCj{}DSKUAWyTK[C[XrHZ^RFZ[[,7HGn\x7fyxowiES}PGWOW\\vL^BN,5JI`}{~iuk{m\x7fRAQMURxNG,3LKsnsjpl~nB{iAMWBP@,2MLpg\x7fa}kEnrjl~PQGG,5JI`}{~iuk{m\x7fTLTVDVWMM,1NMwf|`rjF\x7fm}qk~TD,4KJert|tripAjNVPBTUCC,4KJpo|ksmyoAjNVPBTUCC[+s#,)Vyn`h`fe|,,olbcCt~vz|cz,6ID}u\x7fuuhs@ieg|v@EHZMOY[#s$l$*%s%l%u&k4s&l$l&ms\'l l\'mk"t j\x06l#*%s%l%u&k?s&l#l&ms\'l ,(lfi~ah`{ml\'mk"t j\ufffbl ,(lfi~ah`{m*%s%l%u&kls&l&vr%matchxgr&RegExp$*\\$[a-z]dc_$ n"[!cvk:}l ,(lfi~ah`{ml&m,&efkaoTmk"t j\uffcef z[ cb|1d<,%Dscafgd"in,8[xtm}nLzNEGQMKAdGG^NTY\x1ckgd"inb<b|1d<g,&TboLr{m,(\x02)!jx-2n&vr$testxg,%@tug{mn ,%vrfkbm[!cb|'
+        var _tac = await getTac(uid);
+        if (_tac == null) {
+            res.send(`{"code":-1,"context":"Fail to get tac.","reason":"No such uid ${uid}.Try again if you sure uid is correct."}`);
+        } else {
+            tac = _tac + tac_;
+            var result = await get_signature_for_get(uid, tac);
+            var aweme_list = await get_data_all(uid, result.signature);
+            var data_json = {
+                "code": 0,
+                "total": aweme_list.length,
+                "aweme_list": aweme_list
+            }
+            res.send(JSON.stringify(data_json));
+        }
+    } catch (e) {
+        res.send(e);
+    }
+
+})
+
+
+app.listen(5000, function () {
+    console.log('启动服务端口5000,耐心等待几秒,等待初始化完毕....')
+})
+
+
+async function get_signature_for_get(uid, tac) {
+    console.log('prepare to get signature...')
+    // douyin_2 文件的函数
+    var get_signature = get_sign.get_signature;
+    var base64_tac = Buffer.from(tac, 'utf-8').toString('base64');
+    // 核心
+    var signature = await page.evaluate(get_signature, uid, base64_tac);
+    console.log(signature)
+    return signature;
+};
+
+
+async function getTac(uid) {
+    console.log('getting tac...')
+    try {
+        var url = `https://www.iesdouyin.com/share/user/${uid}`
+        var res = await superagent.get(url)
+            .set({
+                'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36',
+            })
+            .timeout({
+                response: 5000,  // Wait 5 seconds for the server to start sending,
+                deadline: 10000, // but allow 10 seconds for the file to finish loading.
+            })
+            .retry(2);
+        var result = res.text.match(/<script>tac=\'(.*?)\'<\/script>/g)[0];
+        var tac = result.slice(13, result.length - 10);
+        // 代码请求的完整Tac有错误，只取tac一小部分
+        return tac.split('|')[0];
+    } catch (e) {
+        return null;
+    }
+
+}
+
+async function get_data(uid, signature, max_cursor) {
+    try {
+        var url = `https://www.iesdouyin.com/web/api/v2/aweme/post/?user_id=${uid}&sec_uid=&count=21&max_cursor=${max_cursor}&aid=1128&_signature=${signature}&dytk=df4a9c279a56fe0d2bca0d3d98d36320`
+        var res = await superagent.get(url)
+            .set({
+                'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+                // 'accept': 'application/json',
+                // 'accept-encoding': "gzip, br",
+            })
+            .timeout({
+                response: 5000,  // Wait 5 seconds for the server to start sending,
+                deadline: 10000, // but allow 10 seconds for the file to finish loading.
+            })
+            .retry(2);
+        return res.body;
+    } catch (e) {
+        console.log(e)
+        return null;
+    }
+}
+
+
+async function get_data_all(uid, signature) {
+    var max_cursor = 0
+    var aweme_list = [];
+    // 循环max_cursor 得到所有作品信息
+    while (max_cursor !== null) {
+        var temp = await get_data(uid, signature, max_cursor);
+        if (temp == null) {
+            break
+        }
+        aweme_list.push.apply(aweme_list, temp.aweme_list);
+        if (temp.has_more == true) {
+            max_cursor = temp.max_cursor;
+        } else {
+            max_cursor = null;
+        }
+    }
+    return aweme_list;
+}
+
+
