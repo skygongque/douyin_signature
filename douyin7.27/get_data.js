e6711b2db7448858f26ef04d7d153fe057e037e7
william <1243650225@qq.com>
2020-08-01 16:06:15 +0800

    made ua variable

--- douyin7.27/get_data.js
+++ douyin7.27/get_data.js
@@ -5,7 +5,8 @@ async function get_signature(uid,ua) {
     try {
         var res = await superagent.get(`https://www.amemv.com/share/user/${uid}`)
             .set({
-                'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Mobile Safari/537.36'
+                'user-agent':ua,
+                // 'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Mobile Safari/537.36'
             });
         var tac_pattern = new RegExp("<script>tac='(.*?)'</script>");
         
@@ -17,18 +18,20 @@ async function get_signature(uid,ua) {
             return signature;
         }
     }catch(e){
+        console.log(e)
         return null;
     }
 
 };
 
 
-async function get_data(uid, signature, max_cursor) {
+async function get_data(uid, signature,ua, max_cursor) {
     try {
         var url = `https://www.iesdouyin.com/web/api/v2/aweme/post/?user_id=${uid}&sec_uid=&count=21&max_cursor=${max_cursor}&aid=1128&_signature=${signature}&dytk=df4a9c279a56fe0d2bca0d3d98d36320`
         var res = await superagent.get(url)
             .set({
-                'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Mobile Safari/537.36',
+                'user-agent':ua
+                // 'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Mobile Safari/537.36',
                 // 'accept': 'application/json',
                 // 'accept-encoding': "gzip, br",
             })
@@ -47,9 +50,10 @@ async function get_data(uid, signature, max_cursor) {
 !async function main() {
     // var uid = "58841646784"
     var uid = "102064772608"
-    var ua = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Mobile Safari/537.36"
+    var ua = "Mozilla/5.0 ... not verify"
+    // var ua = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Mobile Safari/537.36"
     var signature = await get_signature(uid,ua);
     console.log(signature)
-    var data = await get_data(uid,signature,0);
+    var data = await get_data(uid,signature,ua,0);
     console.log(JSON.stringify(data));
 }()
